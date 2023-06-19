8171ef24154df440216929f2a604cff80851aa46
David Mclean <47851457+davupls@users.noreply.github.com>
2023-06-18 20:04:41 -0500

    Initial Commit

---
+++ myWeather-v2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  myWeather-v2
+//
+//  Created by David Mclean on 6/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myWeather-v2/myWeather_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  myWeather_v2App.swift
+//  myWeather-v2
+//
+//  Created by David Mclean on 6/18/23.
+//
+
+import SwiftUI
+
+@main
+struct myWeather_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
